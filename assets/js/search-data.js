475cbd560ffa6e9e9127bb64645ad4622e412c6e
sigmunjr <sigmunjr@users.noreply.github.com>
2025-04-02 14:00:43 +0000

    Deploying to gh-pages from @ sigmunjr/KI-blogg@acb754862b9b5571e815714cb5662e8ad514d358 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,24 +7,46 @@ ninja.data = [{
     title: "about",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/al-folio/";
+      window.location.href = "/";
     },
   },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/al-folio/blog/";
+            window.location.href = "/blog/";
           },
-        },{id: "post-ockhams-barberkniv-er-en-bærebjelke-for-store-språkmodeller",
+        },{id: "post-selvkjørende-biler-fra-hype-til-virkelighet-snart-i-norge",
         
-          title: "Ockhams barberkniv er en bærebjelke for store språkmodeller",
+          title: "Selvkjørende biler – Fra hype til virkelighet, snart i Norge?",
         
-        description: "I vitenskapen har det lenge vært en grunnregel at de enkleste forklaringene er de beste.",
+        description: "Selvkjørende biler har vært lovet lenge, men selv om mange har gitt opp håpet, blir teknologien forbedret jevnt og trutt.",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/al-folio/blog/2025/ockhams_spraakmodeller/";
+            window.location.href = "/blog/2025/selvkj%C3%B8rende_biler/";
+          
+        },
+      },{id: "post-det-blir-ikke-menneskelig-ki-bare-ved-å-gjette-neste-ord",
+        
+          title: "Det blir ikke menneskelig KI bare ved å gjette neste ord",
+        
+        description: "CEOene i de amerikanske teknologifirmaene sier vi bare trenger større datahaller for at KI skal kunne klare alt, det er nok neppe riktig, men hva skal egentlig til?",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/menneskelig_KI/";
+          
+        },
+      },{id: "post-ockhams-barberkniv-er-en-bærebjelke-for-store-språkmodeller",
+        
+          title: "Ockhams barberkniv er en bærebjelke for store språkmodeller",
+        
+        description: "I vitenskapen har det lenge vært en grunnregel at de enkleste forklaringene er de beste, dette er også mekanismen som gjør at transformer modellene vi finner i ChatGPT fungerer.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/ockhams_spraakmodeller/";
           
         },
       },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
@@ -53,7 +75,7 @@ ninja.data = [{
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/al-folio/books/the_godfather/";
+              window.location.href = "/books/the_godfather/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
@@ -61,7 +83,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/al-folio/news/announcement_2/";
+              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -69,47 +91,47 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/1_project/";
+              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/2_project/";
+              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/3_project/";
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/4_project/";
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/5_project/";
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/6_project/";
+              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/7_project/";
+              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/8_project/";
+              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/9_project/";
+              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
@@ -129,7 +151,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/al-folio/feed.xml", "_blank");
+          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
